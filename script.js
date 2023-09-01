ca6b537924ede2ae05befb8e76825c12567bc5a4
Alpharn <arkrond@gmail.com>
2023-09-01 21:01:19 +0300

    refactor script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 (function () {
   const navButton = document.querySelector("#nav-menu-button");
   const navUl = document.querySelector(".nav-ul");
-  
-  navButton.addEventListener('click', () => {
-    navUl.classList.toggle("hide-ul");
-    navButton.classList.toggle("active");
-  });
-})();
-
-(function () {
   const card = document.querySelector(".card");
   const video = document.querySelector(".card-video");
+  
+  const toggleNav = () => {
+    navUl.classList.toggle("hide-ul");
+    navButton.classList.toggle("active");
+  };
 
-  card.addEventListener('mouseover', () => {
-    video.play();
-  });
+  const playVideo = () => video.play();
+  const pauseVideo = () => video.pause();
 
-  card.addEventListener('mouseout', () => {
-    video.pause();
-  });
+  navButton.addEventListener('click', toggleNav);
+  card.addEventListener('mouseover', playVideo);
+  card.addEventListener('mouseout', pauseVideo);
+
 })();
